e002726ada7ea3511fb41e8cfacd878ddb9030fc
Franco Ortega <francopdx@gmail.com>
2022-01-30 15:22:23 -0800

    update variable names for starting data and state

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,29 @@
 import TileTray from "../tileTray/TileTray";
-// import TileTrayEmpty from "../tileTray/TileTrayEmpty";
-// import totalTiles from '../../data/tiles.json';
-// import tilesArrayData from '../../data/tilesArray.json';
 import tilesObjectData from '../../data/tilesObject.json';
 import tilesObjectHalfData from '../../data/tilesObjectHalf.json';
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [currentTiles, setCurrentTiles] = useState({});
+  const [currentTray, setCurrentTray] = useState({});
 
-  const currentTray = JSON.parse(JSON.stringify(tilesObjectData));
+  const startingTray = JSON.parse(JSON.stringify(tilesObjectData));
 
-  for (const tileSection in currentTray) {
-    currentTray[tileSection].tiles = []
+  // empties tiles from starting tray
+  for (const rowColor in startingTray) {
+    startingTray[rowColor].tiles = []
   }
 
   useEffect(() => {
-    setCurrentTiles(currentTray);
+    setCurrentTray(startingTray);
   }, []);
 
   return (
     <>
       <header><h1>Eclipse Tech Tile Tray</h1></header>
-      <TileTray totalTiles={currentTiles} />
+      <TileTray totalTiles={currentTray} />
       <TileTray
         totalTiles={tilesObjectData}
-        setCurrentTiles={setCurrentTiles}
+        setCurrentTray={setCurrentTray}
         />
       <TileTray totalTiles={tilesObjectHalfData} />
         {/* <TileTrayEmpty /> */}
