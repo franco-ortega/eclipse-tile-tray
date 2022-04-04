cdcdf08a280f707620fe12aa02448b5a63f75c3b
Franco Ortega <francopdx@gmail.com>
2022-04-04 16:22:31 -0700

    remove unused commented out code

--- src/components/tileTray/FullTray.js
+++ src/components/tileTray/FullTray.js
@@ -1,12 +1,10 @@
 import TileRow from "../tileRow/TileRow";
-// import PropTypes from 'prop-types';
 import styles from './TileTray.module.css';
 import { useTrayContext } from "../../state/TrayContext";
 
 const TileTray = () => {
   const { allTiles } = useTrayContext();
   const tileRowList = [];
-  // console.log('CURRENT TILES: ', );
   
   for (const rowName in allTiles) {
     const currentRow = allTiles[rowName]
@@ -35,21 +33,4 @@ const TileTray = () => {
   );
 };
 
-// TileTray.propTypes = {
-//   currentTiles: PropTypes.shape({
-//     // pink: PropTypes.shape({}).isRequired,
-//     // green: PropTypes.shape({}).isRequired,
-//     // tan: PropTypes.shape({}).isRequired,
-//     // black: PropTypes.shape({}).isRequired
-//   }).isRequired,
-// };
-
 export default TileTray;
-
-// const rowObject = {
-//   row: PropTypes.number.isRequired,
-//   slotsPerRow: PropTypes.number.isRequired,
-//   color: PropTypes.string.isRequired,
-//   tiles: PropTypes.array.isRequired
-// };
-// console.log(rowObject);
